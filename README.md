4abecb837921d201d9db5def12750b5d57390982
DruidicBearClan <l.lindquist@live.nl>
2023-03-30 21:33:43 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is my server initialization script
+
+the most basic configs will be generating and settings the default locales as well as configuring ntp to sync with my ntp server
+
+however im sure there will be more stuff down the line to add to this script
---
+++ server-init.sh
@@ -0,0 +1,38 @@
+#Generate default US/NL locales  
+locale-gen en_US.UTF-8
+locale-gen nl_NL.UTF-8
+
+#Update default locales
+update-locale LANG=en_US.UTF-8
+update-locale LC_CTYPE="en_US.UTF-8"
+update-locale LC_NUMERIC=nl_NL.UTF-8
+update-locale LC_TIME=nl_NL.UTF-8
+update-locale LC_COLLATE="en_US.UTF-8"
+update-locale LC_MONETARY=nl_NL.UTF-8
+update-locale LC_MESSAGES="en_US.UTF-8"
+update-locale LC_PAPER=nl_NL.UTF-8
+update-locale LC_NAME=nl_NL.UTF-8
+update-locale LC_ADDRESS=nl_NL.UTF-8
+update-locale LC_TELEPHONE=nl_NL.UTF-8
+update-locale LC_MEASUREMENT=nl_NL.UTF-8
+update-locale LC_IDENTIFICATION=nl_NL.UTF-8
+
+#Set Timezone
+timedatectl set-timezone Europe/Amsterdam
+
+#Disable local timesync daemon
+timedatectl set-ntp off
+
+#Install NTP
+apt install ntp
+
+#Replace contents of /et/ntp.conf
+cat ntp.conf > /etc/ntp.conf
+
+#Restart NTP Service
+service restart ntp
+
+#Check timesync queue
+ntpq -p
+
+
